d0a01f299376829221dbae6d7a9bf0274e9587ec
Sakif <sakifworkspace@gmail.com>
2025-03-15 22:20:51 +0600

    update coffee

--- src/components/UpdateCoffee.jsx
+++ src/components/UpdateCoffee.jsx
@@ -1,10 +1,178 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import Swal from 'sweetalert2';
 
 const UpdateCoffee = () => {
+
+    const coffee = useLoaderData();
+    console.log(useLoaderData)
+    const { _id, name, quantity, supplier,category, taste,details, photo } = coffee;
+
+
+    const handleUpdateCoffee = (event)=>{
+        event.preventDefault();
+
+        const form = event.target;
+        const name = form.name.value;
+        const quantity = form.quantity.value;
+        const supplier = form.supplier.value;
+        const taste = form.taste.value;
+        const category = form.category.value;
+        const details = form.details.value;
+        const photo = form.photo.value;
+    
+        const updateCoffee = {
+          name,
+          quantity,
+          supplier,
+          taste,
+          category,
+          details,
+          photo,
+        };
+        fetch(`http://localhost:5000/coffee/${_id}`,{
+            method:"PUT",
+            headers:{"content-type":"application/json"},
+            body:JSON.stringify(updateCoffee)
+
+        })
+        .then(res=>res.json())
+        .then(data=>{console.log(data)
+          if(data.modifiedCount >0){
+                     Swal.fire({
+                         title: 'Success!',
+                         text: 'Coffee Updated Successfully',
+                         icon: 'success',
+                         confirmButtonText: 'Cool'
+                       })
+                 }
+        })
+      
+    }
     return (
-        <div>
-            <h1>UpdateCoffee</h1>
-        </div>
+        <div className="lg:w-3/4 mx-auto">
+      <div className="text-center p-10">
+        <h1 className="text-5xl font-bold">Update Coffee:{name}</h1>
+        <p className="py-6">
+          Provident cupiditate voluptatem et in.Quaerat fugiat ut assumenda
+          excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a
+          id nisi.
+        </p>
+      </div>
+      <div className="card bg-base-100 w-full shadow-2xl p-8">
+        <form onSubmit={handleUpdateCoffee} className="card-body space-y-6">
+          {/* form first row */}
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-5">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Name</span>
+              </label>
+              <input
+                type="text"
+                name="name"
+                placeholder="Coffee name"
+                defaultValue={name}
+                className="input input-bordered w-full"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Quantity</span>
+              </label>
+              <input
+                type="number"
+                name="quantity"
+                defaultValue={quantity}
+                placeholder="Quantity"
+                className="input input-bordered w-full"
+                required
+              />
+            </div>
+          </div>
+
+          {/* form second row */}
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-5">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Supplier</span>
+              </label>
+              <input
+                type="text"
+                name="supplier"
+                defaultValue={supplier}
+                placeholder="Coffee supplier"
+                className="input input-bordered w-full"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Taste</span>
+              </label>
+              <input
+                type="text"
+                name="taste"
+                defaultValue={taste}
+                placeholder="Taste name"
+                className="input input-bordered w-full"
+                required
+              />
+            </div>
+          </div>
+
+          {/* form third row */}
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-5">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Category</span>
+              </label>
+              <input
+                type="text"
+                name="category"
+                defaultValue={category}
+                placeholder="Coffee Category"
+                className="input input-bordered w-full"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Details</span>
+              </label>
+              <input
+                type="text"
+                name="details"
+                defaultValue={details}
+                placeholder="Coffee Details"
+                className="input input-bordered w-full"
+                required
+              />
+            </div>
+          </div>
+
+          {/* Photo URL */}
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Photo URL</span>
+            </label>
+            <input
+              type="text"
+              name="photo"
+              defaultValue={photo}
+              placeholder="Photo URL"
+              className="input input-bordered w-full"
+              required
+            />
+          </div>
+
+          {/* Submit button */}
+          <div className="form-control mt-6">
+            <button className="btn btn-primary w-full">Update Coffee</button>
+          </div>
+        </form>
+      </div>
+    </div>
     );
 };
 
